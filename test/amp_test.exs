802fb3541c88099358e7618e324c8ffbde97202e
Sam Houston <shouston3@me.com>
2017-04-05 10:31:58 +0100

    adding initial fixtures and initial fixture test

--- test/amp_test.exs
+++ test/amp_test.exs
@@ -5,4 +5,31 @@ defmodule AmpTest do
     {:ok, html, []} = Amp.parse("# Hello World")
     assert html =~ "<h1>Hello World</h1>"
   end
+
+  defp html_ext(name), do: name <> ".html"
+
+  defp remove_white_space(html) do
+    html
+    |> String.split("\n")
+    |> Enum.map(fn string -> String.strip(string) end)
+    |> Enum.join("\n")
+  end
+
+  test "Fixtures" do
+    "test/fixtures/markdown"
+    |> File.ls
+    |> elem(1)
+    |> Enum.each(fn markdown_file ->
+      amp_html_file = String.split(markdown_file, ".")
+        |> hd
+        |> html_ext
+
+      {:ok, markdown} = File.read("test/fixtures/markdown/" <> markdown_file)
+
+      {:ok, actual_html, []} = Amp.parse(markdown)
+      {:ok, expected_html} = File.read("test/fixtures/amp_html/" <> amp_html_file)
+
+      assert remove_white_space(actual_html) == remove_white_space(expected_html)
+    end)
+  end
 end
